58ff9b89314f285a302f7665d6393852e7fb6baa
Aymeric PINEAU <ay.pineau@gmail.com>
2026-04-15 01:50:10 -0600

    refactor: reorganize apps

---
+++ apps-benchmark/tanstack-start-react-static/gt-react-app/vite.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
+import packageJson from "./package.json" with { type: "json" };
+import { resolve } from "path";
+
+const config = defineConfig({
+  resolve: {
+    alias: {
+      "@formatjs/icu-messageformat-parser/printer": resolve(
+        __dirname,
+        "./fix.ts",
+      ),
+    },
+  },
+  plugins: [
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+
+    visualizer({
+      filename: `../../../results/${packageJson.name}/bundle/stats.json`, // Change extension to .json
+      template: "raw-data", // This instructs the plugin to output the data structure instead of HTML
+      gzipSize: true,
+      brotliSize: true,
+    }),
+    visualizer({
+      filename: `../../../results/${packageJson.name}/bundle/rollup-visualizer.html`,
+      open: false,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+    // visualizer({
+    //   filename: `../../../results/${packageJson.name}/bundle/stats-i18n.json`, // Change extension to .json
+    //   template: "raw-data", // This instructs the plugin to output the data structure instead of HTML
+    //   gzipSize: true,
+    //   brotliSize: true,
+    // }),
+    // visualizer({
+    //   filename: `../../../results/${packageJson.name}/bundle/rollup-visualizer-i18n.html`,
+    //   include: [
+    //     { file: "**/node_modules/i18next/**" },
+    //     { file: "**/node_modules/react-i18next/**" },
+    //     { file: "**/node_modules/next-intl/**" },
+    //   ],
+    //   exclude: [
+    //     { file: "**/node_modules/react/**" },
+    //     { file: "**/node_modules/react-dom/**" },
+    //   ],
+    // }),
+  ],
+});
+
+export default config;
